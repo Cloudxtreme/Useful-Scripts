db67e010c5d4397f5ac10b2ca172ee6d21d2c8bc
intlabs <petebirley@gmail.com>
2014-06-09 15:33:01 +0100

    ....

--- OpenStack-Ubuntu-Hostname-Fix.sh
+++ OpenStack-Ubuntu-Hostname-Fix.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 #
 # Ubuntu openstack host fix
+# - fixes the madness that is Canonical not using cloud-init to set the hostname on instance launch.
 #
 # bash <(curl -s https://raw.githubusercontent.com/intlabs/Useful-Scripts/master/OpenStack-Ubuntu-Hostname-Fix.sh)
 
-cp /etc/hosts /tmp/hosts && echo "127.0.1.1 localhost `hostname`">> /tmp/hosts && sudo mv -f /tmp/hosts /etc/hosts
+# We have to do things in this convoluted fashion to stop AppArmor freaking out:
+# we are editing a system file with a cheap script...
+
+cp /etc/hosts /tmp/hosts && \
+echo "127.0.1.1 localhost `hostname`">> /tmp/hosts && \
+sudo mv -f /tmp/hosts /etc/hosts && \
+echo "Hopefully the last time you see the above error message!"
