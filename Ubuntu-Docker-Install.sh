e318cf67eebf557819dd746f482f9f421658103e
intlabs <petebirley@gmail.com>
2014-06-09 15:36:19 +0100

    ...

--- Ubuntu-Docker-Install.sh
+++ Ubuntu-Docker-Install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 #
-# Ubuntu openstack host fix
+# Install docker on Ubuntu 14.04
 #
 # bash <(curl -s https://raw.githubusercontent.com/intlabs/Useful-Scripts/master/Ubuntu-Docker-Install.sh)
 
-sudo apt-get -y update && sudo apt-get -y install docker.io && sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker && sudo restart docker.io
+sudo apt-get -y update && \
+sudo apt-get -y install docker.io && \
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker && \
+sudo restart docker.io
